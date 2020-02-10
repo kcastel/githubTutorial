3b4b786617a651e606d8bbe42c84d48b0dd735d2
kcastel@live.ca <kcastel@live.ca>
2020-02-10 11:26:52 -0800

    fixed function so uses try/except block for matrices of different size

--- myScript.py
+++ myScript.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 my_matrix = np.identity(4)
-my_second_matrix=np.random.random(size=(4,5))
+my_second_matrix=np.random.random(size=(4,4))
 
 def my_function(matrix1,matrix2):
-    return(matrix1*matrix2)
+    try:
+        matrix1*matrix2
+    except ValueError:
+        return None
+
 print(my_function(my_matrix,my_second_matrix))
