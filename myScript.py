5e4142419086925509f9297fda6abb5ac88dce8e
kcastel@live.ca <kcastel@live.ca>
2020-02-10 11:32:01 -0800

    changed the size of the identity matrix and second matrix

--- myScript.py
+++ myScript.py
@@ -1,6 +1,6 @@
 import numpy as np
+import pandas as pd
 
-my_matrix = np.identity(4)
+my_matrix = np.identity(5)
+my_second_matrix = np.random.random(size=(4,5))
 print(my_matrix)
-
-sum(my_matrix)
