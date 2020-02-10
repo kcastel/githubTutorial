5c6c00634fe747f79285aca70f2cba70dba3ea61
Kaity <kcastel@live.ca>
2020-02-10 11:52:08 -0800

    reverted files to 5e4142419086925509f9297fda6abb5ac88dce8e

--- myScript.py
+++ myScript.py
@@ -1,16 +1,6 @@
 import numpy as np
 import pandas as pd
 
-<<<<<<< HEAD
-
-def my_function(matrix1,matrix2):
-    try:
-        matrix1*matrix2
-    except ValueError:
-        print("matrices are not aligned")
-        return None
-
 my_matrix = np.identity(5)
-my_second_matrix = np.random.random(size=(4,4))
-
-print(my_function(my_matrix,my_second_matrix))
+my_second_matrix = np.random.random(size=(4,5))
+print(my_matrix)
