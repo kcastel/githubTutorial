a315da729067f6d5ccdd75dd2c9e408bfcde3a96
kcastel@live.ca <kcastel@live.ca>
2020-02-10 11:21:38 -0800

    created first version of function

--- myScript.py
+++ myScript.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 my_matrix = np.identity(4)
-print(my_matrix)
+my_second_matrix=np.random.random(size=(4,5))
 
-sum(my_matrix)
+def my_function(matrix1,matrix2):
+    return(matrix1*matrix2)
+print(my_function(my_matrix,my_second_matrix))
